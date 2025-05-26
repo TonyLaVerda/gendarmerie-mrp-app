5dd2396147b808e88f8ff8fed6b2e84cc406f388
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-27 00:54:28 +0200

    Update Effectifs.jsx

--- src/pages/Effectifs.jsx
+++ src/pages/Effectifs.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { Card, CardContent } from "../../components/ui/card";
-import { Button } from "../../components/ui/button";
-import { Input } from "../../components/ui/input";
-import { Select, SelectItem } from "../../components/ui/select";
-import { Dialog, DialogTrigger, DialogContent } from "../../components/ui/dialog";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Select, SelectItem } from "../components/ui/select";
+import { Dialog, DialogTrigger, DialogContent } from "../components/ui/dialog";
 
 const grades = ["ELG", "Gnd", "Mdl/C", "ADJ", "Adj/C", "Maj", "Slt", "Lt", "Cpt", "Cen", "Lt Col", "Col"];
 const unites = ["GD", "PMO", "PSIG"];
