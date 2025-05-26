739c3d88e8ea6aac8c9aee68aa8a844109985be6
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-27 01:11:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Effectifs from './pages/Effectifs';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />         {/* Page d'accueil */}
         <Route path="/effectifs" element={<Effectifs />} /> {/* Page Effectifs */}
