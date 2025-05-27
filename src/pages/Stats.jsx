20471d7772ecc6835df26f2d0042d82d25562888
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-27 12:55:53 +0200

    Create Stats.jsx

---
+++ src/pages/Stats.jsx
@@ -0,0 +1,8 @@
+export default function Stats() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Statistiques</h2>
+      <p>Page en cours de construction.</p>
+    </div>
+  );
+}
