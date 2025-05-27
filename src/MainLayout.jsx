bc4145bf109402915fc45ee5ed9fe49a0c263cc2
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-27 12:18:32 +0200

    Update MainLayout.jsx

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -2,20 +2,24 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Effectifs from './pages/Effectifs';
 import Bdsp from './pages/Bdsp';
+import Pulsar from './pages/Pulsar';
 import Navbar from './components/Navbar';
 
 export default function MainLayout() {
   return (
-    <div>
-      <Navbar /> {/* Banniere horizontale */}
-      <div className="p-4 mt-16"> {/* Contenu décalé vers le bas */}
+    <div className="flex flex-col h-screen">
+      <header className="bg-blue-900 text-white shadow">
+        <Navbar />
+      </header>
+
+      <main className="flex-1 overflow-y-auto p-4 bg-gray-50">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/effectifs" element={<Effectifs />} />
           <Route path="/bdsp" element={<Bdsp />} />
-          {/* <Route path="/pulsar" element={<Pulsar />} /> */}
+          <Route path="/pulsar" element={<Pulsar />} />
         </Routes>
-      </div>
+      </main>
     </div>
   );
 }
