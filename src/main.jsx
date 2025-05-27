cce50eac00cd092e1169fecd0a56f332282c9122
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-27 11:56:34 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './AppWrapper';
-import Effectifs from "./pages/Effectifs";
+import AppWrapper from './AppWrapper.jsx'; // ✅ Ajout de l'extension exacte
+// La ligne ci-dessous est inutile ici si tu ne l’utilises pas :
+/* import Effectifs from "./pages/Effectifs"; */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
