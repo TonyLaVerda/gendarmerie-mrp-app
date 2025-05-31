512da520ce642ae3de7a93b0243fa51d8dd2c11c
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-31 20:53:20 +0200

    Update Intervention.js

--- backend/models/Intervention.js
+++ backend/models/Intervention.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const interventionSchema = new mongoose.Schema({
-  titre: String,
-  type: String,
-  unite: String,
-  date: String,
-  heure: String,
-  agents: [String],
-  description: String,
-}, { timestamps: true });
+const interventionSchema = new mongoose.Schema(
+  {
+    titre: { type: String, required: true },
+    type: { type: String, required: true },
+    unite: { type: String, required: true },
+    date: { type: String, required: true },
+    heure: { type: String, required: true },
+    agents: [{ type: String }], // ou ObjectId si tu veux référencer des agents
+    description: { type: String },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Intervention", interventionSchema);
