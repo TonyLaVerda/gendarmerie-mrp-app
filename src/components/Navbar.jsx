0ddb77fbcdcc32b7e4487bb885b52368898ca1a6
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-31 11:02:36 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-// src/components/Navbar.jsx
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import UserMenu from "./UserMenu";
@@ -15,33 +14,30 @@ const links = [
 export default function Navbar() {
   return (
     <header className="navbar">
-      <div className="navbar-header">
+      <div className="navbar-top">
         <div className="navbar-logo-title">
           <img src="/logo-gn.png" alt="Logo GN" className="navbar-logo" />
           <h1 className="navbar-title">Gendarmerie MRP</h1>
         </div>
-        <p className="navbar-tagline">“Notre Engagement, votre sécurité”</p>
+        <UserMenu />
       </div>
 
-      <div className="navbar-menu-container">
-        <nav className="navbar-menu">
-          <ul>
-            {links.map(({ to, label }) => (
-              <li key={to}>
-                <NavLink
-                  to={to}
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  {label}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="navbar-user">
-          <UserMenu />
-        </div>
-      </div>
+      <p className="navbar-tagline">“Notre Engagement, votre sécurité”</p>
+
+      <nav className="navbar-menu">
+        <ul>
+          {links.map(({ to, label }) => (
+            <li key={to}>
+              <NavLink
+                to={to}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
