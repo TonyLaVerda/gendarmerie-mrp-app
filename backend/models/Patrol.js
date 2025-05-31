52d371da737dad339bbf88f541675f09e8df5acf
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-31 20:54:27 +0200

    Update Patrol.js

--- backend/models/Patrol.js
+++ backend/models/Patrol.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const patrolSchema = new mongoose.Schema({
-  type: String,
-  date: String,
-  heure: String,
-  unite: String,
-  agents: [String],
-}, { timestamps: true });
+const patrolSchema = new mongoose.Schema(
+  {
+    type: { type: String, required: true },
+    date: { type: String, required: true },
+    heure: { type: String, required: true },
+    unite: { type: String, required: true },
+    agents: [{ type: String }], // Peut être ObjectId vers "Agent" si besoin
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Patrol", patrolSchema);
