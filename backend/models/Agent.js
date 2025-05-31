d6044de8c8e87cbfbb852ae8327c6a90b6889178
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-31 08:20:30 +0200

    Create Agent.js

---
+++ backend/models/Agent.js
@@ -0,0 +1,12 @@
+// backend/models/Agent.js
+import mongoose from "mongoose";
+
+const agentSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  grade: { type: String, required: true },
+  unite: { type: String, required: true },
+  specialites: { type: [String], default: [] },
+  statut: { type: String, default: "Indispo" },
+}, { timestamps: true });
+
+export default mongoose.model("Agent", agentSchema);
