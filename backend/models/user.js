c44b11ccec7e9d0ff15d1b2566df7a30d8511df0
TonyLaVerda <tonylaverda17@gmail.com>
2025-05-31 10:02:35 +0200

    Create user.js

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+// backend/models/User.js
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["gendarme", "officier"], default: "gendarme" }
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = function (candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+export default mongoose.model("User", userSchema);
